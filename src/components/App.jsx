6c5c4b3fce55fc450cef01f34fab65b8ebc41f9a
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-04-21 11:16:01 +0300

    add react-homework-template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,17 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import { ContactForm } from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
-import { Container } from './App.styled';
-
-const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('Contacts')) ?? [];
-  });
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('Contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = ({ name, number }) => {
-    for (const contact of contacts) {
-      if (contact.name === name) {
-        alert(`${name} is already in contacts.`);
-        return;
-      }
-    }
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    setContacts([contact, ...contacts]);
-  };
-
-  const deleteContact = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
-  };
-
-  const changeFilter = e => {
-    setFilter(e.currentTarget.value);
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  const visibleContacts = getVisibleContacts();
-
+export const App = () => {
   return (
-    <Container>
-      <h1>Phonebook</h1>
-      <ContactForm onSubmitForm={addContact} />
-
-      <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactList onDeleteContact={deleteContact} contacts={visibleContacts} />
-    </Container>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
   );
 };
-
-export default App;
