6a00534bde4100bd88156de1592a5cc167fe6b07
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-04-25 13:42:41 +0300

    add style

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,3 @@
-// import { AppBar } from 'components/AppBar/AppBar';
-// import { Container } from './Layout.styled';
 import { Outlet } from 'react-router-dom';
 import { Navigation } from 'components/Navigation/Navigation';
 
